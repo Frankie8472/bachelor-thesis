f3557cfea029ed935d6e497e37220a0bd5030f0a
Frankie <knobelf@student.ethz.ch>
2019-07-28 01:41:44 +0200

    changed some stuff

---
+++ src/game.ts
@@ -0,0 +1,18 @@
+import "phaser";
+import { GameScene } from "./gameScene";
+import GameConfig = Phaser.Types.Core.GameConfig;
+
+const config: GameConfig = {
+    title: "BatArang",
+    width: 800,
+    height: 600,
+    parent: "game",
+    scene: [GameScene],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
+    backgroundColor: "#000033"
+};
